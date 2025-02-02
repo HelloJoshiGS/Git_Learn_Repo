db5b1536e7fbf1016b9a41c687b47fd5d6c71947
Ghanshyam <ghanshyam.joshi@citytech.global>
2025-02-02 15:10:12 +0545

    commit to GS

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,3 +5,11 @@ if answer == True:
     print("One")
 else:
     print("Two")
+
+value = ["Branch", "BranchTwo"]
+
+for i in value:
+    if "Branch" in value:
+        print("Success")
+    else:
+        print("Not Success")
